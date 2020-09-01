f0fe162edbd88c75d22d12321baae2f824056d2a
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-08-31 19:04:08 -0700

    fix: use vscode to install extensions
    (rather than copying the extension from the folder directly)

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 
 	"github.com/pelletier/go-toml"
-
-	"github.com/otiai10/copy"
 )
+
+func ensureLength(arr []string, minLength int, message string) {
+	if len(arr) < minLength {
+		log.Fatalln(message)
+	}
+}
 
 func contains(arr []string, query string) bool {
 	for _, el := range arr {
@@ -21,10 +26,20 @@ func contains(arr []string, query string) bool {
 	return false
 }
 
+// Extension gives information about a particular vscode extension
+type Extension struct {
+	Name string   `json:"name"`
+	Tags []string `json:"stags"`
+}
+
+// Config gives information about the whole configuration file
+type Config struct {
+	Version    string      `json:"version"`
+	Extensions []Extension `json:"extension"`
+}
+
 func main() {
 	args := os.Args[1:]
-
-	first := args[0]
 
 	if contains(args, "--help") || contains(args, "-h") {
 		fmt.Println(`sparta
@@ -36,85 +51,56 @@ Commands:
     generate
     Generates all extension folders to be used by vscode
 
-    lauch [workspace]
+    launch [workspace]
     launches a particular workspace in vscode`)
 		os.Exit(0)
 	}
 
-	if first == "generate" {
-		type Extension struct {
-			Name string   `json:"name"`
-			Tags []string `json:"stags"`
-		}
+	ensureLength(args, 1, "Must specify command. Exiting")
 
-		type Extensions struct {
-			Version    string      `json:"version"`
-			Extensions []Extension `json:"extension"`
-		}
+	command := args[0]
+	if command == "generate" {
+		var config Config
 
-		var extensionsToml Extensions
-
-		t, err := ioutil.ReadFile(filepath.Join("extensions.toml"))
+		configRaw, err := ioutil.ReadFile(filepath.Join("extensions.toml"))
 		if err != nil {
 			panic(err)
 		}
-		if err = toml.Unmarshal(t, &extensionsToml); err != nil {
+		if err = toml.Unmarshal(configRaw, &config); err != nil {
 			panic(err)
 		}
 
-		for _, extension := range extensionsToml.Extensions {
-			fmt.Printf("processing %s\n", extension.Name)
+		for _, extension := range config.Extensions {
+			fmt.Printf("EXTENSION: %s\n", extension.Name)
+
 			for _, tag := range extension.Tags {
-				fmt.Printf("foo %s\n", tag)
-				if err := copy.Copy(filepath.Join("extensions", extension.Name), filepath.Join("workspaces", tag, extension.Name)); err != nil {
+				fmt.Printf("tag: %s\n", tag)
+
+				// install extension
+				extensionsDir := filepath.Join("workspaces", tag)
+
+				cmd := exec.Command("code", "--extensions-dir", extensionsDir, "--install-extension", extension.Name, "--force")
+				cmd.Stderr = os.Stderr
+				stdout, err := cmd.Output()
+				if err != nil {
 					panic(err)
 				}
+				fmt.Println(string(stdout))
 			}
 		}
+	} else if command == "launch" {
+		ensureLength(args, 2, "Must pass in a workspace name")
+		workspaceName := args[1]
+		extensionsDir := filepath.Join("workspaces", workspaceName)
 
-		fmt.Println(string(t))
-	} else if first == "workspace" {
-		second := args[1]
-		name := args[2]
-
-		if name == "" {
-			fmt.Println("need to pass in a workspace name")
-			os.Exit(1)
-		}
-
-		if second == "create" {
-			if err := os.MkdirAll(filepath.Join("workspaces", name), os.ModePerm); err != nil {
-				panic(err)
-			}
-		} else if second == "delete" {
-			if name == "" {
-				fmt.Println("Must pass in a workspace name")
-				os.Exit(1)
-			}
-
-			if err := os.RemoveAll(filepath.Join("workspaces", name)); err != nil {
-				panic(err)
-			}
-		}
-	} else if first == "launch" {
-		name := args[1]
-
-		if name == "" {
-			fmt.Println("need to pass in a workspace name")
-			os.Exit(1)
-		}
-
-		extensionsDir := filepath.Join("workspaces", name)
 		cmd := exec.Command("code", "--extensions-dir", extensionsDir, ".")
 		stdout, err := cmd.Output()
-
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(stdout)
 	} else {
-		fmt.Println("unknown option")
-		os.Exit(1)
+		log.Fatalln("Unknown Command. Exiting")
 	}
 
 }
