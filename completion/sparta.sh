b152f90240899e2f15c2b45897442d42c5e0d245
Edwin Kofler <24364012+eankeen@users.noreply.github.com>
2020-09-28 22:14:26 -0700

    add bash autocomplete

---
+++ completion/sparta.sh
@@ -0,0 +1,120 @@
+#
+# shellcheck shell=bash
+#
+
+# based on https://tylerthrailkill.com/2019-01-19/writing-bash-completion-script-with-subcommands/
+
+_log() {
+	local -r file=".sparta-debug"
+	[[ -v DEBUG ]] && {
+		"$@" >> "$file"
+	} 1>&2 2>/dev/null
+}
+
+_sparta_launch() {
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	local dirs
+
+	# TODO
+	dirs="$(\ls --color=never -c1 ~/.cache/sparta/workspaces)"
+	# shellcheck disable=SC2207
+	COMPREPLY=($(compgen -W "$dirs" -- "$cur"))
+}
+
+_sparta_plumbing ()
+{
+  local i=1 subcommand_index
+
+  while [[ $i -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[i]}"
+    case "$s" in
+    subcommand)
+	 	# we set the current subcommand
+      subcommand_index=$i
+      break
+      ;;
+    esac
+
+    (( i++ ))
+  done
+
+  while [[ $subcommand_index -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[subcommand_index]}"
+    case "$s" in
+      download-extensions)
+        COMPREPLY=()
+        return
+        ;;
+		remove-extensions)
+			COMPREPLY=()
+			return
+			;;
+		symlink-extensions)
+			COMPREPLY=()
+			return
+			;;
+      remove-symlinks)
+        COMPREPLY=()
+        return
+        ;;
+    esac
+    (( subcommand_index++ ))
+  done
+
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+	# shellcheck disable=SC2207
+  COMPREPLY=($(compgen -W "download-extensions remove-extensions symlink-extensions remove-symlinks" -- "$cur"))
+}
+
+_sparta() {
+	local i=1 cmd
+
+	# iterate over COMP_WORDS (ending at currently completed word)
+	# this ensures we get command completion even after passing flags
+	while [[ "$i" -lt "$COMP_CWORD" ]]; do
+		local s="${COMP_WORDS[i]}"
+		case "$s" in
+		# if our current word starts with a '-', it is not a subcommand
+		-*) ;;
+		# we are completing a subcommand, set cmd
+		*)
+			cmd="$s"
+			break
+			;;
+		esac
+		(( i++ ))
+	done
+
+	# check if we're completing 'sparta'
+	if [[ "$i" -eq "$COMP_CWORD" ]]; then
+		local cur="${COMP_WORDS[COMP_CWORD]}"
+		# shellcheck disable=SC2207
+		COMPREPLY=($(compgen -W "init update check launch plumbing -h --help" -- "$cur"))
+		return
+	fi
+
+	# if we're not completing 'sparta', then we're completing a subcommand
+	case "$cmd" in
+		init)
+			COMPREPLY=()
+			;;
+		update)
+			COMPREPLY=()
+			;;
+		check)
+			COMPREPLY=()
+			;;
+		launch)
+			_sparta_launch
+			;;
+		plumbing)
+			_sparta_plumbing
+			;;
+		*)
+			;;
+	esac
+
+  return 0
+}
+
+complete -F _sparta sparta
